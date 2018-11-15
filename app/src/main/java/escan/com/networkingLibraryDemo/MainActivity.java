3c2e0628334e3138cf325a0f3d25343b0fb2665b
ajinkya <ajinkya@escanav.com>
2018-11-15 18:49:16 +0530

    Log verbose message added for debug

--- app/src/main/java/escan/com/networkingLibraryDemo/MainActivity.java
+++ app/src/main/java/escan/com/networkingLibraryDemo/MainActivity.java
@@ -2,12 +2,17 @@ package escan.com.networkingLibraryDemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.v(TAG,"onCreate method");
     }
 }
